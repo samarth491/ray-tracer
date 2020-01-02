87a4c2b12adc1bc42f9a353e74126c627b0913fa
samarth491 <setu.samarth@gmail.com>
2020-01-02 17:51:30 +0530

    added a general Hitable class to make hitable objects like spheres. Also added a Hitable_list class which stores all the objects in the scene.

---
+++ hitable_list.hpp
@@ -0,0 +1,35 @@
+#ifndef HITABLELISTH
+#define HITABLELISTH
+
+#include "hitable.h"
+
+class hitable_list: public hitable {
+public:
+	hitable_list() {}
+	hitable_list(hitable **l, int n) {
+		list = l;
+		size = n;
+	}
+
+	virtual bool hit(const ray &r, float tmin, float tmax, hit_record &rec) const;
+
+	hitable **list;
+	int size;
+};
+
+bool hitable_list::hit(const ray &r, float tmin, float tmax, hit_record &rec) const {
+	hit_record tmp;
+	bool did_hit = false;
+	double closest = tmax;
+
+	for(int i=0;i<size;i++) {
+		if(list[i]->hit(r, tmin, closest, tmp)) {
+			closest = tmp.t;
+			rec = tmp;
+			did_hit = true;
+		}
+	}
+	return did_hit;
+}
+
+#endif
